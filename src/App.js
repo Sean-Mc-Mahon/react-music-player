7bb5f62833ede0ff51f84513de6e65daf28a7dbc
Sean-Mc-Mahon <68223484+Sean-Mc-Mahon@users.noreply.github.com>
2021-07-01 15:24:53 +0100

    song data added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useState } from "react";
+//Import Styles
+import "./styles/app.scss";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+//Import Util
+import data from "./util";
+
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[3]);
   return (
     <div className="App">
       <h1>Music Player</h1>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
